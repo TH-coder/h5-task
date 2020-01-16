8b7936afb437f4dae88abd7237ba599e6e2771d7
chen <578159913@qq.com>
2020-01-16 23:02:28 +0800

    增加奖项概率

--- lottery.js
+++ lottery.js
@@ -1,5 +1,3 @@
-
-
 class Lottery {
     constructor(container, data) {
         const lottery = document.createElement("div");
@@ -10,7 +8,7 @@ class Lottery {
                 const startBtn = document.createElement("div");
                 startBtn.className = "lottery-item lottery-start";
                 startBtn.innerText = "开始";
-                startBtn.addEventListener("click",()=>{
+                startBtn.addEventListener("click", () => {
                     this.run();
                 })
                 lottery.appendChild(startBtn);
@@ -18,9 +16,14 @@ class Lottery {
             const itemDiv = document.createElement("div");
             itemDiv.className = "lottery-item";
             itemDiv.innerText = item.name;
-            items.push(itemDiv);
+            const obj = {
+                node: itemDiv,
+                percent: item.percent || undefined
+            }
+            items.push(obj);
             lottery.appendChild(itemDiv);
         })
+        container.appendChild(lottery);
 
         this.items[7] = items[2];
         this.items[6] = items[4];
@@ -31,25 +34,47 @@ class Lottery {
         this.items[1] = items[0];
         this.items[0] = items[1];
 
-        container.appendChild(lottery);
+        let restItemTotal = 0;
+        const rest = this.items.reduce((sum, item, index) => {
+            if (item.percent || item.percent === 0) {
+                item.percent = item.percent * 100;
+                sum -= item.percent;
+            } else {
+                restItemTotal++;
+            }
+            item.index = index;
+            return sum
+        }, 10000)
+
+        this.items.filter(item => !item.percent && item.percent !== 0).forEach((item) => item.percent = Math.round(rest / restItemTotal))
+
+        this.items.reduce((sum, item) => {
+            item.floor = sum;
+            item.ceil = sum + item.percent;
+            return sum + item.percent;
+        }, 0)
+
     }
     items = [];
     onRun = false;
 
     run() {
-        if(this.onRun)return;
+        if (this.onRun) return;
         this.onRun = true;
         let times = 100,
-            num = Math.floor(Math.random() * 8),
+            num,
             interval = 500;
+
+        const random = Math.floor(Math.random() * 10000);
+
+        num = this.items.filter(item=>item.floor<=random && random<item.ceil)[0].index;
 
         const fn = () => {
             times--;
             this.items.forEach(element => {
-                element.className = "lottery-item";
+                element.node.className = "lottery-item";
             });
-
-            this.items[times % 8].className = "lottery-item active";
+            this.items[times % 8].node.className = "lottery-item active";
             if (times > 40 && interval > 100) {
                 interval -= 30;
             }
